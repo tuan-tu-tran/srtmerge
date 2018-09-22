f6d7272f0c016904424c0f2f67546282c3073ddc
Tuan-Tu Tran <tuantu.t@gmail.com>
2018-09-22 02:28:17 +0200

    read a sample file

--- LibSrtMerge.Tests/TestParser/TestParser.cs
+++ LibSrtMerge.Tests/TestParser/TestParser.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using NUnit.Framework;
 
 namespace Tests
@@ -12,6 +14,18 @@ namespace Tests
         [Test]
         public void Test1()
         {
+            var parser = new SubtitlesParser.Classes.Parsers.SrtParser();
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            Console.WriteLine(String.Join(",", assembly.GetManifestResourceNames()));
+            using (var stream = assembly.GetManifestResourceStream("LibSrtMerge.Tests.TestParser.sample.srt"))
+            {
+                using(var reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+                
+            }
+            // parser.ParseStream();
             Assert.Pass();
         }
     }
