d5aa5f7fe341020ce492c35e1a149de4a8e3848b
Tuan-Tu Tran <tuantu.t@gmail.com>
2018-10-08 00:49:27 +0200

    test the command line

---
+++ LibSrtMerge.Tests/TestCommandLineInterface/TestCommandLine.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+
+namespace LibSrtMerge
+{
+    [TestFixture]
+    public class TestCommandLine
+    {
+        [Test]
+        public void ItCanMergeSrtFiles()
+        {
+            var fsMock = new Mock<IFileSystem>(MockBehavior.Strict);
+            fsMock.Setup(fs => fs.File.OpenRead(It.IsAny<string>())).Returns<string>(Tests.TestParser.GetResourceStream);
+            var output = new MemoryStream();
+            fsMock.Setup(fs => fs.File.OpenWrite("output.srt")).Returns(output);
+
+            var cli = new CommandLineInterface()
+            {
+                FileSystem = fsMock.Object,
+            };
+
+            cli.Main(new string[]{
+                "sample3.srt", "sample4.srt"
+            });
+            Assert.That(Encoding.UTF8.GetString(output.ToArray()), Is.EqualTo(Tests.TestParser.ReadResourceContent("sample10.srt")));
+        }
+    }
+}
