59834cee2a8d1d5828ceb3c6ed90416e71072d87
Tuan-Tu Tran <tuantu.t@gmail.com>
2018-10-08 00:57:54 +0200

    new console app

---
+++ CAppSrtMerge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CAppSrtMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
