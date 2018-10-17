bf2056a2813a9dacec4abad1e324b070b598e990
Tuan-Tu Tran <tuantu.t@gmail.com>
2018-10-17 20:25:09 +0200

    test identify non-zip file

--- LibSrtMerge.Tests/TestZipFileProcessor/TestZipFileProcessor.cs
+++ LibSrtMerge.Tests/TestZipFileProcessor/TestZipFileProcessor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using NUnit.Framework;
 
 namespace LibSrtMerge.Tests.TestZipFileProcessor
@@ -6,12 +7,16 @@ namespace LibSrtMerge.Tests.TestZipFileProcessor
     [TestFixture]
     public class TestZipFileProcessor
     {
+        Stream GetStream(string name) => Geekality.IO.EmbeddedResource.Get<TestZipFileProcessor>(name, true);
+
         [Test]
-        public void ItCanIdentifyZipFile()
+        [TestCase("sample1.zip", true)]
+        [TestCase("sample2.srt", false)]
+        public void ItCanIdentifyZipFile(string name, bool isZipFile)
         {
-            var stream = Geekality.IO.EmbeddedResource.Get<TestZipFileProcessor>("sample1.zip", true);
+            var stream = Geekality.IO.EmbeddedResource.Get<TestZipFileProcessor>(name, true);
             var processor = new ZipFileProcessor();
-            Assert.That(processor.IsZipFile(stream), Is.True);
+            Assert.That(processor.IsZipFile(stream), Is.EqualTo(isZipFile));
         }
 
     }
