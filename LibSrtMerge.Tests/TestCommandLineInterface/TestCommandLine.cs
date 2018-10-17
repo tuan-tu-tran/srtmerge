6e6b6c7837360dc6aa023724a0eb86bb4299c789
Tuan-Tu Tran <tuantu.t@gmail.com>
2018-10-17 22:34:10 +0200

    refactored a test
    duplicate the resources

--- LibSrtMerge.Tests/TestCommandLineInterface/TestCommandLine.cs
+++ LibSrtMerge.Tests/TestCommandLineInterface/TestCommandLine.cs
@@ -5,16 +5,17 @@ using Moq;
 using NUnit.Framework;
 using Tests;
 
-namespace LibSrtMerge
+namespace LibSrtMerge.Tests.TestCommandLineInterface
 {
     [TestFixture]
     public class TestCommandLine
     {
+        Stream GetStream(string name) => Geekality.IO.EmbeddedResource.Get<TestCommandLine>(name, true);
         [Test]
         public void ItCanMergeSrtFiles()
         {
             var fsMock = new Mock<IFileSystem>(MockBehavior.Strict);
-            fsMock.Setup(fs => fs.File.OpenRead(It.IsAny<string>())).Returns<string>(TestParser.GetResourceStream);
+            fsMock.Setup(fs => fs.File.OpenRead(It.IsAny<string>())).Returns<string>(GetStream);
             var output = new MemoryStream();
             fsMock.Setup(fs => fs.File.Open("output.srt", FileMode.Create)).Returns(output);
 
@@ -26,7 +27,7 @@ namespace LibSrtMerge
             cli.Main(new string[]{
                 "sample3.srt", "sample4.srt"
             });
-            Assert.That(Encoding.UTF8.GetString(output.ToArray()), Is.EqualTo(TestParser.ReadResourceContent("sample10.srt")));
+            Assert.That(output.ToArray(), Is.EqualTo(GetStream("sample10.srt").ReadBytes()));
         }
     }
 }
